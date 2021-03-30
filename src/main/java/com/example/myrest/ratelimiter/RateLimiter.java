c1b1ec122b4e34181fcf9f6a5808f43a0a290bfc
stevenbg <stevenbg@gmail.com>
2021-03-30 17:17:21 +0300

    finish the rate limiter

---
+++ src/main/java/com/example/myrest/ratelimiter/RateLimiter.java
@@ -0,0 +1,80 @@
+package com.example.myrest.ratelimiter;
+
+import io.github.bucket4j.*;
+
+import javax.cache.Cache;
+import javax.cache.CacheManager;
+import javax.cache.configuration.MutableConfiguration;
+import javax.cache.expiry.AccessedExpiryPolicy;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * A generic rate limiter. Requires a CacheManager.
+ */
+public class RateLimiter {
+//    to change the params on the fly,
+//    we'd have to replaceConfiguration for all the buckets,
+//    while moving them in a new cache with the new expiry
+    private final Integer rate;
+    private final Integer interval;
+//    we're using Cache, so the entries can expire
+//    or we'd have to write a gc to prevent the ConcurrentHashMap from growing indefinitely
+    private Cache<String, Bucket> cache;
+
+    static private AtomicInteger instanceCounter = new AtomicInteger(0);
+
+    /**
+     * @param rate number of requests
+     * @param interval period in seconds
+     * @param cacheManager the app's CacheManager
+     */
+    public RateLimiter(Integer rate, Integer interval, CacheManager cacheManager) {
+        this.rate = rate;
+        this.interval = interval;
+
+//        keeping stale buckets past interval is pointless
+        MutableConfiguration<String, Bucket> configuration =
+                new MutableConfiguration<String, Bucket>()
+                        .setTypes(String.class, Bucket.class)
+                        .setStoreByValue(false)
+                        .setStatisticsEnabled(false)
+                        .setExpiryPolicyFactory(
+                                AccessedExpiryPolicy.factoryOf(new javax.cache.expiry.Duration(TimeUnit.SECONDS, interval)));
+
+        String cache_name = getClass().getCanonicalName() + String.valueOf(instanceCounter.getAndIncrement());
+        cache = cacheManager.createCache(cache_name, configuration);
+    }
+
+    private Bucket makeBucket(String key) {
+        Refill refill = Refill.intervally(rate, Duration.ofSeconds(interval));
+        Bandwidth limit = Bandwidth.classic(rate, refill);
+        return Bucket4j.builder()
+                .addLimit(limit)
+                .build();
+    }
+
+    public Result attemptAction(String actor_key) {
+//        wasteful compared to computeIfAbsent
+        cache.putIfAbsent(actor_key, makeBucket(actor_key));
+        Bucket tokenBucket = cache.get(actor_key);
+
+        ConsumptionProbe probe = tokenBucket.tryConsumeAndReturnRemaining(1);
+        Result result = null;
+
+        return new Result(probe.isConsumed(), probe.getRemainingTokens(), probe.getNanosToWaitForRefill() / 1_000_000_000);
+    }
+
+    public class Result {
+        public final Boolean success;
+        public final Long remaining;
+        public final Long wait;
+
+        public Result(Boolean success, Long remaining, Long wait) {
+            this.success = success;
+            this.remaining = remaining;
+            this.wait = wait;
+        }
+    }
+}
--- src/main/java/com/example/myrest/ratelimiter/RateLimiterInterceptor.java
+++ src/main/java/com/example/myrest/ratelimiter/RateLimiterInterceptor.java
@@ -1,63 +1,23 @@
 package com.example.myrest.ratelimiter;
 
-import io.github.bucket4j.*;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-import javax.cache.Cache;
 import javax.cache.CacheManager;
-import javax.cache.Caching;
-import javax.cache.configuration.MutableConfiguration;
-import javax.cache.expiry.AccessedExpiryPolicy;
-import javax.cache.expiry.ModifiedExpiryPolicy;
-import javax.cache.spi.CachingProvider;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.time.Duration;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
-// todo extract RateLimiter
-/**
- * Fat interceptor
- */
+
 public class RateLimiterInterceptor implements HandlerInterceptor {
 
-//    to change the params on the fly,
-//    we'd have to replaceConfiguration for all the buckets,
-//    while moving them in a new cache with the new expiry
-    private final Integer rate;
-    private final Integer interval;
-//    we're using Cache, so the entries can expire
-//    or we'd have to write a gc to prevent the ConcurrentHashMap from growing indefinitely
-    private Cache<String, Bucket> cache;
-
-    static private AtomicInteger instanceCounter = new AtomicInteger(0);
+    private final RateLimiter rateLimiter;
 
     /**
      * Returns a rate limiting interceptor
      * @param rate number of requests
      * @param interval period in seconds
+     * @param cacheManager the app CacheManager
      */
     public RateLimiterInterceptor(Integer rate, Integer interval, CacheManager cacheManager) {
-        this.rate = rate;
-        this.interval = interval;
-
-//        keeping stale buckets past interval is pointless
-        MutableConfiguration<String, Bucket> configuration =
-                new MutableConfiguration<String, Bucket>()
-                        .setTypes(String.class, Bucket.class)
-                        .setStoreByValue(false)
-                        .setStatisticsEnabled(false)
-                        .setExpiryPolicyFactory(AccessedExpiryPolicy.factoryOf(new javax.cache.expiry.Duration(TimeUnit.SECONDS, interval)));
-
-        cache = cacheManager.createCache("ratelimiter" + instanceCounter.getAndIncrement(), configuration);
-    }
-
-    private Bucket makeBucket(String key) {
-        Refill refill = Refill.intervally(rate, Duration.ofSeconds(interval));
-        Bandwidth limit = Bandwidth.classic(rate, refill);
-        return Bucket4j.builder()
-                .addLimit(limit)
-                .build();
+        rateLimiter = new RateLimiter(rate, interval, cacheManager);
     }
 
     @Override
@@ -66,19 +26,14 @@ public class RateLimiterInterceptor implements HandlerInterceptor {
 
 //        should cover X-Forwarded-For
         String key = request.getRemoteAddr();
-//        wasteful compared to computeIfAbsent
-        cache.putIfAbsent(key, makeBucket(key));
-        Bucket tokenBucket = cache.get(key);
+        RateLimiter.Result test = rateLimiter.attemptAction(key);
 
-        ConsumptionProbe probe = tokenBucket.tryConsumeAndReturnRemaining(1);
-        if (probe.isConsumed()) {
-            response.addHeader("X-Rate-Limit-Remaining", String.valueOf(probe.getRemainingTokens()));
+        if (test.success) {
+            response.addHeader("X-Rate-Limit-Remaining", String.valueOf(test.remaining));
             return true;
         }
         else {
-            Long waitForRefill = probe.getNanosToWaitForRefill() / 1_000_000_000;
-            response.addHeader("X-Rate-Limit-Retry-After-Seconds", String.valueOf(waitForRefill));
-
+            response.addHeader("X-Rate-Limit-Retry-After-Seconds", String.valueOf(test.wait));
             throw new RateLimiterException("Exhausted API quota for " + key);
         }
     }
