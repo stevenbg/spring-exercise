f2e496bd801178b9bf9810ce680c4255e0656452
Steven <stevenbg@gmail.com>
2021-06-15 17:58:39 +0300

    first try

--- run-docker-mysql.sh
+++ run-docker-mysql.sh
@@ -2,7 +2,7 @@
 
 wehere=$(cd "$(dirname "${BASH_SOURCE[0]}")" || exit; pwd -P)
 
-docker run --name=mysql --rm -d -p 127.0.0.1:43306:3306 \
+sudo podman run --name=mysql --rm -d -p 127.0.0.1:43306:3306 \
   -v "${wehere}/dev/initdb.sql:/docker-entrypoint-initdb.d/initdb.sql" \
   -v "/var/lib/mysql" \
   mysql/mysql-server:8.0
--- src/main/java/com/example/myrest/system/AppConfig.java
+++ src/main/java/com/example/myrest/system/AppConfig.java
@@ -3,7 +3,6 @@ package com.example.myrest.system;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
